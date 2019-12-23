e85705e69d95a7e588327ae9b9b3768853d35148
Vahid Dzanic <vdc@gmx.ch>
2019-12-23 17:05:26 +0000

    Use target_compile_features in order to fix issue when qt5 adding std=gnu++11 as public flag

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -8,20 +8,24 @@ int main(int argc, char* argv[])
   int returncode = 0;
   if (argc != 1)
   {
+    bool server = false;
     if (std::string(argv[1]) == "server")
     {
-      QApplication a(argc, argv);
-      MainWindow w(true);
-      w.show();
-      returncode = a.exec();
+      server = true;
     }
     else if (std::string(argv[1]) == "client")
     {
-      QApplication a(argc, argv);
-      MainWindow w(false);
-      w.show();
-      returncode = a.exec();
+      server = false;
     }
+    else
+    {
+      std::cerr << "define first argument \"client\" or \"server\"" << std::endl;
+      return 1;
+    }
+    QApplication a(argc, argv);
+    MainWindow w(server);
+    w.show();
+    returncode = a.exec();
   }
   return returncode;
 }
